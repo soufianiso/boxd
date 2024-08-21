4a642187c730c1b25aad54db88810637da85f2e4
soufianiso <soufiantamasi@gmail.com>
2024-08-21 16:24:42 +0200

    add writejson to handle erros

--- services/user/routes.go
+++ services/user/routes.go
@@ -1,10 +1,12 @@
 package user
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/soufianiso/boxd/utils"
+	"github.com/soufianiso/boxd/types"
 )
 
 // "database/sql"
@@ -23,13 +25,22 @@ func NewHandler(storage Store) *Handler {
 
 
 func(h *Handler) SetRoutes(r *mux.Router) *mux.Router{
-	r.HandleFunc("/user", h.userhandle )
+	r.HandleFunc("/user", utils.MiddlewearApi(h.userhandle)).Methods("POST")
 	return r
 
 }
 
-func(h *Handler) userhandle(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w,"ss")
+func(h *Handler) userhandle(w http.ResponseWriter, r *http.Request) error{
+
+	user := types.User{}
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil{
+		return err
+	}
+
+	utils.WriteJson(w, http.StatusOK , user) 
+
+	return nil
 
 }
 
