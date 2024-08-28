c044b8821d305db681079442a0e8683a434a76b6
soufianiso <soufiantamasi@gmail.com>
2024-08-28 10:58:44 +0200

    some changes

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,6 @@ import (
 	"database/sql"
 	"log"
 	"os"
-	"fmt"
-
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/soufianiso/boxd/cmd/api"
@@ -16,32 +14,23 @@ func main(){
 	godotenv.Load()
 	conn := os.Getenv("postgres")
 	dbchannel := make(chan *sql.DB)
-	fmt.Println("main function start ")	
 
 	go func (){
-		newdb, err := sql.Open("postgres",conn)	
+		db, err := sql.Open("postgres",conn)	
 		if err != nil{
 			log.Fatal(err)
 		}
-		dbchannel <- newdb 
-
-		
+		dbchannel <- db
 	}()
 
-	db := <- dbchannel
+	db := <-dbchannel
 	defer db.Close()
 
 	if err := db.Ping() ; err != nil{
 		log.Fatal(err)
 	}
 
-	log.Printf("connection success")
-	
 	app := api.NewServer(":8000",db) 
 	app.Run()
-
-
-	
-
 }
 
--- services/user/routes.go
+++ services/user/routes.go
@@ -29,20 +29,21 @@ func NewHandler(storage Store) *Handler {
 
 
 func(h *Handler) SetRoutes(r *mux.Router) *mux.Router{
-
 	r.HandleFunc("/login", utils.ErrorHandler(h.handleLogin)).Methods("POST")
 	r.HandleFunc("/register", utils.ErrorHandler(h.handleRegister)).Methods("POST")
-	return r
 
+	return r
 }
 
 func(h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) error{
 	user := new(types.User)
 
+	// Convert request body to object
 	if err := json.NewDecoder(r.Body).Decode(&user) ; err != nil {
 		return err
 	}
 
+	//create Signed the jwt token and create it 
 	secretkey := "secret"
 	tokenString, err := auth.Createjwt(user.Username, secretkey)
 	if err != nil{
@@ -50,15 +51,16 @@ func(h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) error{
 	}
 
 	return utils.WriteJson(w, http.StatusOK , map[string]string{"Authorization": tokenString}) 
-
 }
 
 func(h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) error{
 	user := new(types.User)
+	// Convert request body to object
 	if err := json.NewDecoder(r.Body).Decode(user) ; err != nil{
 		return err
 	}
 	
+	// Checking whether the email exists or not
 	_ , err := h.storage.GetUserByEmail(user.Username)
 	if err ==  nil{
 		return utils.WriteError(w,http.StatusBadRequest, utils.ApiError{ 
@@ -66,18 +68,17 @@ func(h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) error{
 		})
 	}
 
+	// hash the password
 	hashedPassword, err := auth.HashPassword(user.Password)
 	if err != nil{
 		return err
 	}
-
+	
 	if err := h.storage.CreateUser(user.Username, hashedPassword) ; err != nil {
 		return err
 	}
 	
-	
 	return utils.WriteJson(w, http.StatusCreated, map[string]string{"status":"created"})
-
 }
 
 
