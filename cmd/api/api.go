8529f3f870eb07831282dd637382d8449ee8b25a
soufianiso <soufiantamasi@gmail.com>
2024-09-06 23:17:28 +0200

    add subroute

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -6,24 +6,25 @@ import (
 
 	"github.com/gorilla/mux"
 
-	// "github.com/gorilla/handlers"
 	"github.com/soufianiso/boxd/services/user"
 	"github.com/soufianiso/boxd/utils"
 )
 
 func NewServer(db *sql.DB) http.Handler{
 	router := mux.NewRouter()
+	apiRouter := router.PathPrefix("/api/v1").Subrouter()
 
 	// user service	
 	userstore := user.NewStorage(db)
 	userHandler := user.UserHandler(userstore) 
-	userHandler.SetRoutes(router)
+	userHandler.SetRoutes(apiRouter)
 
 
 	// here is top level middleware stuff
 	handler := utils.CORSMiddleware(router)
 	return handler
 }
+
 
 
 
