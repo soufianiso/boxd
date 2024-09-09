f453d023a5a1bcc9a91e2bf2d08ecbcc309a179b
soufianiso <soufiantamasi@gmail.com>
2024-09-09 15:40:48 +0200

    refactoring

--- services/user/routes.go
+++ services/user/routes.go
@@ -1,43 +1,33 @@
 package user
 
 import (
-	"encoding/json"
 	"log"
-
 	"github.com/redis/go-redis/v9"
 	"github.com/soufianiso/boxd/types"
-
 	"os"
-
 	"github.com/joho/godotenv"
-
-	// "encoding/json"
 	"net/http"
-
 	"github.com/gorilla/mux"
 	"github.com/soufianiso/boxd/auth"
 	"github.com/soufianiso/boxd/utils"
 )
+ 
 
-func SetRoutes(
-	r *mux.Router, 
-	storage Store, 
-	logger *log.Logger, 
-	redisClient *redis.Client,
-)	{
+// Routes
+func SetRoutes( r *mux.Router, storage Store, logger *log.Logger, redisClient *redis.Client,) {
 	r.Handle("/login", handleLogin(storage ,logger)).Methods("POST")
 	r.Handle("/register", handleRegister(storage, logger)).Methods("POST")
 	}
 
+// Handlers
 func handleLogin(storage Store, logger *log.Logger) http.Handler{
 	godotenv.Load()
 	jwtsecret := os.Getenv("jwtsecret")
 	
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user := new(types.User)
-		if err := json.NewDecoder(r.Body).Decode(&user) ; err != nil {
-			logger.Println("failed decoding",err)
-			return 
+		if err := utils.Decode(r, user) ; err != nil{
+			logger.Println(err)
 		}
 
 		u , err := storage.GetUserByEmail(user.Email)
@@ -46,7 +36,6 @@ func handleLogin(storage Store, logger *log.Logger) http.Handler{
 			utils.WriteError(w,http.StatusBadRequest, utils.ApiError{ Error: "email or password is incorrect" })
 			return 
 		}
-
 
 		if !auth.ComparePasswords(u.Password, []byte(user.Password)) {
 			logger.Println(err)
@@ -60,27 +49,26 @@ func handleLogin(storage Store, logger *log.Logger) http.Handler{
 			return 
 		}
 
-		w.Header().Set("Authorization", "Bearer "+tokenString)	
-		utils.WriteJson(w, http.StatusOK , map[string]string{"Authorization": tokenString}) 
-
+		err = utils.Encode(w, r, http.StatusCreated, map[string]string{"Authorization": tokenString}) 
+		if err != nil{
+			logger.Println(err)
+		}
 	})
 }
 
 func handleRegister(storage Store, logger *log.Logger) http.Handler{
-	user := new(types.User)
-
+	
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		
-		if err := json.NewDecoder(r.Body).Decode(&user) ; err != nil{
+		user := new(types.User)
+		if err := utils.Decode(r, user) ; err != nil{
 			logger.Println(err)
-			return 
 		}
-		
+
 		// Checking whether the email exists or not
 		_ , err := storage.GetUserByEmail(user.Email)
 		if err ==  nil{
 			logger.Println("email already exists")
-			utils.WriteError(w,http.StatusBadRequest, utils.ApiError{ Error: "email already exists"})
+			utils.WriteError(w, http.StatusBadRequest, utils.ApiError{ Error: "email already exists"})
 			return
 		}
 
@@ -96,7 +84,10 @@ func handleRegister(storage Store, logger *log.Logger) http.Handler{
 			return
 		}
 		
-		utils.WriteJson(w, http.StatusCreated, map[string]string{"status":"created"})
+		err = utils.Encode(w, r, http.StatusCreated, map[string]string{"status":"created"}) 
+		if err != nil{
+			logger.Println(err)
+		}
 
 	})
 }
--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -45,4 +46,19 @@ func CORSMiddleware(next http.Handler) http.Handler {
 
         next.ServeHTTP(w, r)
     })
+}
+func Encode(w http.ResponseWriter, r *http.Request, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Decode(r *http.Request, v any) (error) {
+	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+		return  fmt.Errorf("decode json: %w", err)
+	}
+	return  nil
 }
